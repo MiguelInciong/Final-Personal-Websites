7ceb9e228d5088a82f33782de876bfbb8e33e46e
MiguelInciong <116319179+MiguelInciong@users.noreply.github.com>
2022-12-14 22:41:03 +0800

    Add files via upload

---
+++ Inciong Personal Websites/script.js
@@ -0,0 +1,14 @@
+
+document.getElementById("submit").onclick = function(){
+    let myName = document.getElementById("name").value;
+    alert("Hello, " + myName + "!")
+}
+
+function subscribe(){
+    alert("HELLO! welcome to my youtube channel! press OK to continue")
+}
+
+
+function social(){
+    alert("You are about to go into my socials! press OK to continue")
+}
